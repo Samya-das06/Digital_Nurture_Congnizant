3eafb946d15ce1008df719a3ba3b26a27d4c4010
Samya Das <samyadas773@gmail.com>
2025-07-17 18:55:15 +0530

    commit message

---
+++ JWT_Authentication_File/Secure_Controller.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Runtime.InteropServices;
+
+namespace JwtAuthApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class SecureController : ControllerBase
+{
+    [Authorize]
+    [HttpGet("data")]
+    public IActionResult GetSecretData()
+    {
+        return Ok("This is protected data accessible only with a valid JWT.");
+    }
+}
